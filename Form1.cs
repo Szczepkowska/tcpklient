965750a3050b883f84dd143bffdbff4529772eb4
student <student@PC-213>
2017-05-24 16:32:50 +0200

    dzialajaca aplikacja -klienttcp-wersja1

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace tcpklient
 {
@@ -15,6 +16,27 @@ namespace tcpklient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void przycisk_polacz_Click(object sender, EventArgs e)
+        {
+            string host = adres.Text;
+            int port = System.Convert.ToInt16(my_port.Value);
+            try
+
+            {
+                TcpClient klient = new TcpClient(host, port);
+                info_o_polaczeniu.Items.Add("Nawiazano polaczenie z " + host + " na porcie :" + port);
+                klient.Close();
+            }
+            catch (Exception ex)
+            {
+                info_o_polaczeniu.Items.Add("Blad:" + "Nie udalo sie nawiazac polaczenia");
+                MessageBox.Show(ex.ToString(), "Blad");
+               }
+            }
+
+
         }
     }
 }
